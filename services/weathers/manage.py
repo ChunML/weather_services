7277b85afc105c4ae0ae0a8afeb1bf04398c659e
Trung Tran <trungtran.chunml@gmail.com>
2019-09-06 16:01:50 +0900

    Add test cli command

--- services/weathers/manage.py
+++ services/weathers/manage.py
@@ -3,6 +3,8 @@ from project import create_app, db
 from project.api.models import Weather
 from project.utils.download_and_prepare import get_weather_from_date, clean_data
 from sqlalchemy.exc import IntegrityError
+import sys
+import unittest
 
 
 app = create_app()
@@ -18,7 +20,7 @@ def recreate_db():
 
 @cli.command('seed_db')
 def seed_db():
-    df = get_weather_from_date(2014, 1, 1)
+    df = get_weather_from_date(2019, 8, 1)
     df = clean_data(df)
     dict_df = df.to_dict('index')
     try:
@@ -27,6 +29,15 @@ def seed_db():
     except IntegrityError:
         db.session.rollback()
         print('Something just went wrong when populating past data!')
+
+
+@cli.command()
+def test():
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    sys.exit(result)
 
 
 if __name__ == '__main__':
